bab81fff243850005e2a39ab875e26f619a0da56
Misael Alejandro Hernandez Mendoza <a2223410342@alumnos.uat.edu.mx>
2024-05-29 20:55:18 -0500

    Inicio_repositorio

---
+++ code/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var resultado int
+	// fmt.Println("Escoje que Operacion te gustaria hacer")
+	// fmt.Println("1. Suma")
+	// fmt.Println("2, Resta")
+	// fmt.Println("3. Multiplicacion")
+	// fmt.Println("4. Division")
+	// var opcion int8 = 1
+	// num_1 := 10
+	// num_2 := 39
+
+	// if opcion == 1 && num_2 >= 20 {
+	// 	resultado = num_1 + num_2
+	// 	fmt.Println(resultado)
+	// }
+
+	// if opcion == 1 {
+	// 	resultado = num_1 * num_2
+	// 	fmt.Println(resultado)
+	// }
+
+	a:= "Hola Mundo"
+	b:= "¿Como estan?"
+
+	resultado:= a + "" + b
+
+	fmt.Println(resultado)
+
+}
